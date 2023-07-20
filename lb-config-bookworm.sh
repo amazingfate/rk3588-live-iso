96c1609290785f2e8423b7dfd0d35d4845684797
jfliu <jfliu@zshield.net>
2023-07-20 12:19:15 +0800

    fix missing files

--- lb-config-bookworm.sh
+++ lb-config-bookworm.sh
@@ -43,7 +43,7 @@ wget https://raw.githubusercontent.com/armbian/build/main/config/cli/common/main
 wget https://raw.githubusercontent.com/armbian/build/main/config/cli/common/main/packages.additional -O config/package-lists/armbian-cli-addtional.list.chroot
 wget https://raw.githubusercontent.com/armbian/build/main/config/desktop/bullseye/environments/gnome/config_base/packages -O config/package-lists/armbian-gnome.list.chroot
 sed -i "/lightdm/d" config/package-lists/armbian-gnome.list.chroot
-cp additional-packages.bookworm config/package-lists/additional-packages.list.chroot
+cp additional-packages config/package-lists/additional-packages.list.chroot
 
 mkdir -p config/includes.chroot_after_packages/etc/netplan
 cp networkmanager.yaml config/includes.chroot_after_packages/etc/netplan
